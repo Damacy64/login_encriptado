140f5088c8f8cdb9eacd6772a93771fe7e6f6aa9
Damacy64 <80659223+Damacy64@users.noreply.github.com>
2025-02-09 22:49:01 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+    <form action="post">
+        
+            <h2>Inicio Sesion</h2>
+            <div class="form__input">
+                <input type="email" name="correo" placeholder="Correo">
+            </div>
+            <div class="form__input">
+                <input type="text" name="contrasenia" placeholder="Contraseña">
+            </div>
+            <input class="boton" type="submit" name="ingresar" value="Ingresar">
+            <a href="registrar.php">Registrarse</a>
+            <a href="restaurar.php">Restaurar</a>
+        
+    </form>
+</body>
+</html>
